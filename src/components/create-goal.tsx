3da02964438cf0e43689c65b62763b181c071c31
Hebert S. Pereira <hbrt.spx@gmail.com>
2024-09-16 09:50:40 -0300

    adjust for mobile / create goals

--- src/components/create-goal.tsx
+++ src/components/create-goal.tsx
@@ -154,7 +154,7 @@ export function CreateGoal() {
               />
             </div>
           </div>
-          <div className="flex items-center gap-3">
+          <div className="flex items-center gap-7">
             <DialogClose asChild>
               <Button type="button" variant="secondary" className="flex-1">
                 Fechar
