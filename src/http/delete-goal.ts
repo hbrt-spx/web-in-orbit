4f7d682216323682351783bd4c4dd92c04086f06
Hebert S. Pereira <hbrt.spx@gmail.com>
2024-10-02 09:05:47 -0300

    feat: add route delete goal

--- src/http/delete-goal.ts
+++ src/http/delete-goal.ts
@@ -1,13 +1,13 @@
-export async function deleteGoal(goalId: string) {
-  const webService = import.meta.env.VITE_WEBSERVICE_URL
+// export async function deleteGoal(goalId: string) {
+//   const webService = import.meta.env.VITE_WEBSERVICE_URL
 
-  await fetch(`${webService}/delete-goal`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      goalId,
-    }),
-  })
-}
+//   await fetch(`${webService}/delete-goal`, {
+//     method: 'DELETE',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       goalId,
+//     }),
+//   })
+// }
